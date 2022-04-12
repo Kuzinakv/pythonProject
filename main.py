fc49226221978ac1b98f5d6b5c740cc48a8c3548
Kuzinakv <kuzina112210@gmail.com>
2022-04-13 00:46:31 +0300

    1

---
+++ main.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name, a, b):
+    # Use a breakpoint in the code line below to debug your script.
+    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    a = b * 3
+    b = 3 * 45
+    a = a + b
+    #print(a + b / 42)
+    return 0
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    jopa = print_hi('ks', 35, 64)
+    print(jopa)
+    print_hi('ks', 2, 4)
+    print_hi('ks', 3, 5)
+
+def print_hi2(name, a, b):
+    # Use a breakpoint in the code line below to debug your script.
+    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    a = b * 3
+    b = 3 * 45
+    a = a + b
+    print(a + b / 42)
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
